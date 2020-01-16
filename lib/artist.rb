d3c56ab2f8848700a71c6e09dad2ce6deafb987f
dwane richards <dwanerich@gmail.com>
2020-01-16 01:58:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,16 @@
+class Artist
+  attr_accessor :name
+  def intitialize(name)
+    @name = name
+  end
 
+
+class Song
+  attr_accessor :title
+
+  def intitialize(title)
+    @title = title
+  end
+end
+
+end
